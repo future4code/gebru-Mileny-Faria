245c9713c5f8626f357c0fd2e6807b0a64a8d2c8
milenyfaria <myrfaria@gmail.com>
2022-02-24 21:36:32 -0300

    começo exercico labenusers

---
+++ modulo3/apis-labenusers/labenusers/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CadastroUsuario from './components/CadastroUsuario';
+import './App.css';
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <CadastroUsuario />
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ modulo3/apis-labenusers/labenusers/src/components/CadastroUsuario.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class CadastroUsuario extends React.Component {
+
+    render () {
+      return (
+        <div>
+          <button>Trocar de Tela</button>
+          <input placeholder='Nome' />
+          <input placeholder='Email' />
+          <button>Criar Usuário</button>
+  
+        </div>
+      );
+    }
+  }
+  
+  export default CadastroUsuario;
+  
