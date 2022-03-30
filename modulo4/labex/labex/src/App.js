bbdeb4ab6964ef3595f9ad6630295f98d05b3408
milenyfaria <myrfaria@gmail.com>
2022-03-29 21:08:44 -0300

    criando react e pages

---
+++ modulo4/labex/labex/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { HomePage } from "./pages/HomePage";
+
+export const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
---
+++ modulo4/labex/labex/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LabeX from "../img/LabeX.png";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <img src={LabeX} />
+    </div>
+  )
+}
