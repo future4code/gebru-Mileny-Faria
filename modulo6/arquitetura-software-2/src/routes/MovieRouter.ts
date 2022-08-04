1a66e48679d7a98935c592bc9b21e5fc2a6fd7df
milenyfaria <myrfaria@gmail.com>
2022-08-03 21:02:16 -0300

    Exercise 2

---
+++ modulo6/arquitetura-software-2/src/routes/MovieRouter.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+
+export const MovieRouter = express.Router()
+
+// const movieController = new UserController()
+
+MovieRouter.get('/getAll', )
+MovieRouter.post("/create", )
