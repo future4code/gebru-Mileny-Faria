c2cd740d4121c73d1dd55fb476b59fa891daec38
milenyfaria <myrfaria@gmail.com>
2022-09-05 22:19:35 -0300

    exercicios

--- modulo7/testes-unitarios-mooks/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { app } from './controller/app'
-import { recipeRouter } from './controller/RecipeRouter'
-import { userRouter } from './controller/UserRouter'
-
-
-app.use('/user/', userRouter)
-app.use('/recipe/', recipeRouter)
--- modulo7/testes-unitarios-mooks/src/services/IdGenerator.ts
+++
@@ -1,7 +0,0 @@
-import { v4 } from 'uuid'
-
-class IdGenerator {    
-    generateId = () => v4() 
-} 
-
-export default new IdGenerator()
