a214b8aa1eb1da49f46a1f7bd74dcd0c504f60f6
milenyfaria <myrfaria@gmail.com>
2022-09-19 20:13:40 -0300

    criando header

--- case1-promobit/promobit/src/App.js
+++ case1-promobit/promobit/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Router } from './routes/Router'
+import { Header } from './components/Header/Header'
 
 export const App = () => {
   return (
     <div>
+      <Header />
       <Router />
     </div>
   )
