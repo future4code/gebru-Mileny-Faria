a7243ac844fb5124fb9500c3a52b217fdd31d013
milenyfaria <myrfaria@gmail.com>
2022-09-20 20:14:14 -0300

    colocando botões de navegação

--- case1-promobit/promobit/src/pages/HomePage/HomePage.js
+++ case1-promobit/promobit/src/pages/HomePage/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { goToDetailsMoviePage } from '../../routes/coordinator'
 
 export const HomePage = () => {
+    const navigate = useNavigate()
+
     return (
         <div>
-            Home Page
+            <h1>Home Page</h1>
+            <button onClick={() => goToDetailsMoviePage(navigate)}>Detalhes do Filme</button>
         </div>
     )
 }
