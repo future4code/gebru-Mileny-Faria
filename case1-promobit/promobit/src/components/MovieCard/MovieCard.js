a4b1e738dd06cc2e321e117e3cef75bc52386e27
milenyfaria <myrfaria@gmail.com>
2022-09-21 20:31:32 -0300

    editando fotos dos filmes

--- case1-promobit/promobit/src/components/MovieCard/MovieCard.js
+++ case1-promobit/promobit/src/components/MovieCard/MovieCard.js
@@ -2,16 +2,11 @@ import React from 'react'
 
 export const MovieCard = (props) => {
   return (
-    <div onClick={props.onClick}>
-      <img
-        component={"img"}
-        alt={props.title}
-        height={"220px"}
-        image={props.image}
-        title={props.title}
-      />
+    <div key={props.id}>
+      <img src={props.img} />
       <div>
         <h5>{props.title}</h5>
+        <h3>{props.date}</h3>
       </div>
     </div>
   )
--- case1-promobit/promobit/src/pages/HomePage/HomePage.js
+++ case1-promobit/promobit/src/pages/HomePage/HomePage.js
@@ -8,18 +8,31 @@ import { BASE_URL } from '../../constants/urls'
 
 export const HomePage = () => {
     const navigate = useNavigate()
-    const [ movies, isLoading ] = useRequestData([], `${BASE_URL}/movie/popular?api_key=0b14b275632acd0fc1cf3dedac88afaa`)
+    const apiKey = '0b14b275632acd0fc1cf3dedac88afaa'
+    const [ movies, isLoading ] = useRequestData([], `${BASE_URL}/movie/popular?api_key=${apiKey}`)
+
+    // base_url: http://image.tmdb.org/t/p/
+    // size: "w45" "w92" "w154" "w185" "w300" "w500" "original"
+    // file_path:
+    // https://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
+
+    console.log(movies)
 
     const renderedMovies = movies.results && movies.results.map((movie) => {
+        const baseUrl = 'http://image.tmdb.org/t/p/'
+        const size = 'w92'
+
         return (
             <MovieCard
                 id={movie.id}
-                img={movie.poster_path}
+                img={`${baseUrl}${size}${movie.poster_path}`}
                 title={movie.title}
                 data={movie.release_date}
             />
         )
     })
+
+    // console.log(movies)
 
     return (
         <div>
