277f31ce8e71583d53081f843bc613b879048a47
milenyfaria <myrfaria@gmail.com>
2022-09-21 19:11:49 -0300

    estilizando header

--- case1-promobit/promobit/src/components/Header/Header.js
+++ case1-promobit/promobit/src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { HeaderContainer, ImageLogo } from './styled'
+import Vector from '../../assets/Vector.png'
 
 export const Header = () => {
     return(
-        <div>
-            HEADER
-        </div>
+        <HeaderContainer>
+            <ImageLogo src={Vector}/>
+        </HeaderContainer>
     )
 }
